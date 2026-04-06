ee5075115cb7449fe6b98b124b89f9356b968f0e
arbi222 <arbihamolli111@gmail.com>
2026-04-06 14:36:31 +0200

    updating axios

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: import.meta.env.VITE_API_KEY,
-    baseURL: "/",
+    baseURL: import.meta.env.VITE_API_KEY,
     withCredentials: true
 });
 
